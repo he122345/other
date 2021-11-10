5eab3d8ceeec975ad282661f0523fea07aa26763
helong <1169104403@qq.com>
2021-11-10 10:02:46 +0800

    1

--- python/testweb/testapp/tests.py
+++ python/testweb/testapp/tests.py
@@ -1,25 +1,46 @@
-# 4.3
-a, b = eval(input("Please input two integer (use , to separate):\n"))
-
-if a != b:
-
-    if a > b:
-        big = a
-        small = b
-    else:
-        big = b
-        small = a
-
-    while 1:
-        c = big % small
-        if c != 0:
-            big = small
-            small = c
-            continue
-        else:
-            break
-else:
-    small = a
-
-print("最大公约数为 {}".format(small))
-print("最小公倍数为 {:d}".format(int(a * b / small)))
+students = [
+    {'name': '张三', 'age': 18, 'score': 98, 'tel': '1388888998', 'gender': 'female'},
+    {'name': '李四', 'age': 28, 'score': 95, 'tel': '1388666666', 'gender': 'male'},
+    {'name': '王五', 'age': 21, 'score': 98, 'tel': '1365588889', 'gender': 'unknown'},
+    {'name': 'chris', 'age': 17, 'score': 47, 'tel': '13777775523', 'gender': 'male'},
+    {'name': 'jack', 'age': 23, 'score': 52, 'tel': '13999999928', 'gender': 'female'},
+    {'name': 'tony', 'age': 15, 'score': 89, 'tel': '1388888888', 'gender': 'unknown'}]
+count = 0
+for x in students:
+    if x['score'] < 60:
+        count += 1
+print("不及格学生的个数:", count)
+print("不及格学生的名字和对应的成绩:", end="")
+for x in students:
+    if x['score'] < 60:
+        print(x['name'], x['score'], end=",")
+print()
+count = 0
+for x in students:
+    if x['age'] < 18:
+        count += 1
+print("未成年学生的个数:", count)
+print("手机尾号是8的学生的名字:", end="")
+for x in students:
+    if int(x['tel'][-1]) == 8:
+        print(x['name'], end=",")
+print()
+m = 0
+print("最高分和对应的学生的名字:", end="")
+for x in students:
+    if m < x['score']:
+        m = x['score']
+for x in students:
+    if m == x['score']:
+        print(x['name'], x['score'], end=",")
+print()
+for x in students:
+    if x['gender'] == "unknown":
+        students.remove(x)
+print("删除性别不明的所有学生:")
+for x in students:
+    print(x)
+s=sorted(students, key=lambda x: x['score'], reverse=True)
+print("将列表按学生成绩从大到小排序:")
+for x in s:
+    print(x)
